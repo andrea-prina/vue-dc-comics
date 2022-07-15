e936f746c928e50a870f79f00fb0af71e8c2b4e1
Andrea Prina <and.prina@gmail.com>
2022-07-15 16:52:27 +0200

    feat: Create first footer layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,58 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-4">ELEMENTO PICCOLO</div>
-                <div class="col-8">ELEMENTO GRANDE</div>
+                <div class="col-4">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                                <h6>SHOP</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                            </div>
+                            <div class="col-4">
+                                <h6>DC COMICS</h6>
+                                <ul class="list-unstyled">
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                    <li>ITEM</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8 text-end">
+                    LOGO
+                </div>
             </div>
         </div>
     </footer>
@@ -17,6 +67,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+footer {
+    background-image: url("../assets/img/footer-bg.jpg");
+    color: white;
+    padding: 2rem 0;
+    font-size: 0.75rem;
+}
 
 </style>
