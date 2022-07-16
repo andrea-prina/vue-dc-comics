3c814131d4b2a5d34b9c6368646db172c293d984
Andrea Prina <and.prina@gmail.com>
2022-07-16 11:36:29 +0200

    feat: Add static elements to action bar

--- src/components/ActionsBar.vue
+++ src/components/ActionsBar.vue
@@ -2,11 +2,11 @@
     <div class="action-bar">
         <div class="container">
             <div class="row p-5">
-                <div class="col text-center"><img src="../assets/img/buy-comics-digital-comics.png" alt="">ITEM</div>
-                <div class="col text-center"><img src="" alt="">ITEM</div>
-                <div class="col text-center"><img src="" alt="">ITEM</div>
-                <div class="col text-center"><img src="" alt="">ITEM</div>
-                <div class="col text-center"><img src="" alt="">ITEM</div>
+                <div class="col text-center"><img src="../assets/img/buy-comics-digital-comics.png" alt="">DIGITAL COMICS</div>
+                <div class="col text-center"><img src="../assets/img/buy-comics-merchandise.png" alt="">DC MERCHANDISE</div>
+                <div class="col text-center"><img src="../assets/img/buy-comics-subscriptions.png" alt="">SUBSCRIPTIONS</div>
+                <div class="col text-center"><img src="../assets/img/buy-comics-shop-locator.png" alt="">COMIC SHOP LOCATOR</div>
+                <div class="col text-center"><img src="../assets/img/buy-dc-power-visa.svg" alt="">DC POWER VISA</div>
             </div>
         </div>
     </div>
@@ -23,9 +23,11 @@ export default {
 
     .action-bar{
         background-color: $brand-color;
+        color: #FFF;
 
         img {
             height: 3rem;
+            margin-right: 0.5rem;
         }
     }
 
