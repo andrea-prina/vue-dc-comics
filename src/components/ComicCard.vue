7f080988d6da96112a2e3ef13da00da280652448
Andrea Prina <and.prina@gmail.com>
2022-07-18 15:23:45 +0200

    feat: Add card component to dinamically display comics and info

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="comic-card">
+        <img :src="thumbImg" alt="">
+        <h5>{{ comicPrice }}</h5>
+        <h5>{{ comicSeries }}</h5>
+        <h5>{{ comicType }}</h5>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    props: {
+        'thumbImg' : String,
+        'comicPrice' : String,
+        'comicSeries' : String,
+        'comicType' : String,
+    }
+}
+</script>
+
+<style>
+
+</style>
